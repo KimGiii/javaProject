87040dc39c37ec118275c2b9aa790d23e256fa41
Jiwon6889 <119488116+Jiwon6889@users.noreply.github.com>
2022-12-14 07:55:26 +0900

    Update Main.java

--- src/RythmeGame/Main.java
+++ src/RythmeGame/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	public static final int FRAME_HEIGHT = 720;
 	public static final int NOTE_SPEED = 5;
 	public static final int SLEEP TIME = 10;
+	public static final int REACH_TIME = 1; //노트 생성후 판정선에 도착할때까지의 시간.
+	
 	
 	public static void main(String args[]) {
 		
