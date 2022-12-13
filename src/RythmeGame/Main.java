9abbf2b78ad803257f9b643e69528ffc94695c45
Jiwon6889 <119488116+Jiwon6889@users.noreply.github.com>
2022-12-14 06:47:43 +0900

    Update Main.java

--- src/RythmeGame/Main.java
+++ src/RythmeGame/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	
 	public static final int FRAME_WIDTH = 1280;
 	public static final int FRAME_HEIGHT = 720;
+	public static final int NOTE_SPEED = 5;
+	public static final int SLEEP TIME = 10;
 	
 	public static void main(String args[]) {
 		
